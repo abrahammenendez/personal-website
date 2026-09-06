523a02c81e65c3d09cb7c77f626d3dd20c8667ec
Abraham Menéndez <menendezabraham@gmail.com>
2026-09-06 20:45:25 +0200

    chore: fix commitlint

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -2,10 +2,7 @@ export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [2, 'always', ['feat', 'fix', 'revert', 'chore']],
-    'type-case': [2, 'always', 'lower-case'],
     'scope-case': [2, 'always', 'lower-case'],
     'subject-case': [2, 'always', 'lower-case'],
-    'subject-empty': [2, 'never'],
-    'subject-full-stop': [2, 'never', '.'],
   },
 }
